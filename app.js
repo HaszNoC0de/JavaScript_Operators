0bb61483ef222944223ac7361ebcd8414158fe02
Hasheem Reddick <hasznoneother@gmail.com>
2022-11-06 15:21:27 -0500

    updated

--- app.js
+++ app.js
@@ -6,14 +6,48 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let sum = a + b;
+let subtract = a - b;
+let division = a / b;
+console.log(sum, subtract, division,);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+//num + str = "1111"
+//num + str2 = "11eleven"
+//num + isPresent = 12
+//firstName + num = Frodo11
+// isPresent + str = true11
+//firstName + lastName = FrodoBaggins
+
+
+
 
 // YOUR CODE HERE
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+//What is the value of: val == str3? true
+//What is the value of: val === str3? false
+//What is the value of: !isPresent2? true
+//What is the value of: (“eleven” == str4 && val >= str3)? false
+//What is the value of: (!isPresent2 || isPresent2)? true
+//What is the value of: 0 == false? true
+//What is the value of: 0 === false? false
+//What is the value of: 0 != false? false
+//What is the value of 0 !== false? true
 
 // YOUR CODE HERE
